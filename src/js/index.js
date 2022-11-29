ba25a2889ce7949feef83b6b19e96c2eb2792ac6
Latorreander <latorreander@gmail.com>
2022-11-28 21:47:34 -0300

    "responsivo e ajustado validação"

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,17 @@ const botao = document.getElementById("botao");
 
 botao.addEventListener("click", function () {
   campoFormulario.forEach(function (area) {
-    if (area.value == "") {
+    if (area.value === ""){
       area.nextElementSibling.classList.add("ativo");
       area.classList.add("nao-preenchido");
-    } else area.classList.add("preenchido");
+    } else if(area.value != ""){
+      area.nextElementSibling.classList.remove("ativo")
+      area.classList.add("preenchido")
+      area.classList.remove("nao-preenchido")
+    }
   });
 });
+
+
+
+
